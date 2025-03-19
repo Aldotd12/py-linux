e19b373cd543db54028c6ef207c0435b6ec274b4
Aldotd12 <tolentinodomingodiego@gmail.com>
2025-03-19 17:24:35 -0600

    Revisando el hosteo de la BD

--- config/db.py
+++ config/db.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:aldo@localhost:3306/test"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+load_dotenv()
+engine = create_engine(os.getenv("SQLALCHEMY_DATABASE_URL")) #apunta al .env
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base = declarative_base()
--- main.py
+++ main.py
@@ -30,4 +30,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
